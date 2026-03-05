6c23669b95ce0a260a8b1a20b8a2f6bb37cf8bfa
moabia1 <mdmoabia81@gmail.com>
2026-03-05 15:23:04 +0530

    auth flow completed

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,121 @@
+const userModel = require("../models/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
+const tokenBlacklistModel = require("../models/blacklist.model")
+/**
+ * @name Register Controller
+ * @description Register a new user, expects username, email and password in the request body, returns a JWT token if successful
+ * @access Public
+ */
+async function registerController(req, res) {
+  const { username, email, password } = req.body
+  
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      message: "fill the all fields"
+    })
+  }
+
+  const isUserExists = await userModel.findOne({
+    $or: [{ username: username }, { email: email }]
+  })
+
+  if (isUserExists) {
+    return res.status(400).json({
+      message: "user already exists"
+    })
+  }
+
+  const hash = await bcrypt.hash(password, 10)
+  
+  const user = await userModel.create({
+    username,
+    email,
+    password:hash  
+  })
+
+  const token = jwt.sign({
+    id: user._id,
+    username: user.username
+  }, process.env.JWT_SECRET, { expiresIn: "1d" })
+  
+  res.cookie("token", token)
+  
+  return res.status(201).json({
+    message: "User registered successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email:user.email
+    }
+  })
+}
+
+
+
+/**
+ * @name loginUserController
+ * @description Login a user, expects email and password in the request body, returns a JWT token if successful
+ * @access Public
+ */
+async function loginController(req, res) { 
+  const { email, password, username } = req.body
+  
+  const user = await userModel.findOne({
+    $or:[{email:email},{username:username}]
+  })
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid credentials"
+    })
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password)
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message:"Invalid email or password"
+    })
+  }
+
+  const token = jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    {expiresIn:"1d"}
+  )
+
+  res.cookie("token", token)
+  
+  return res.status(201).json({
+    message: "User login successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+}
+
+
+async function logoutController(req,res) {
+  const token = req.cookies.token
+  if (!token) {
+    return res.status(400).json({
+      message: "token is required to logout"
+    })
+  }
+
+  await tokenBlacklistModel.create({ token })
+  
+  res.clearCookie("token")
+
+  res.status(200).json({
+    message: "User logged out successfully"
+  })
+}
+
+module.exports = {
+  registerController,
+  loginController,
+  logoutController
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,29 @@
 const express = require("express")
+const {registerController,loginController, logoutController} = require("../controllers/auth.controller")
 
 const authRouter = express.Router()
 
-authRouter.post("/register",)
+/**
+ * @route POST /auth/register
+ * @desc Register a new user
+ * @access Public
+ */
+authRouter.post("/register", registerController)
+
+
+/**
+ * @route POST /auth/login
+ * @desc Login a user
+ * @access Public
+ */
+authRouter.post("/login", loginController)
+
+
+/**
+ * @route GET /auth/logout
+ * @desc Logout a user, expects a valid JWT token in the request header, adds the token to the blacklist
+ * @access Private
+ */
+authRouter.get("/logout",logoutController)
 
 module.exports = authRouter
