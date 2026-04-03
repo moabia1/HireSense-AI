5cc27798b54cb19d5f6b1980cab6b315313eb8c9
moabia1 <mdmoabia81@gmail.com>
2026-04-03 16:22:09 +0530

    implementing structured output process

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/db")
 
+
 connectToDB()
 app.listen(3000, () => {
   console.log(`Server running on port 3000`)
